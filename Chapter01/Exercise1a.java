fb281b6d3e494fd6dd65bf38543dcea02be26c21
Heuristic <codemonkey2468@hotmail.com>
2020-09-30 16:08:35 -0400

    add another file

---
+++ Chapter01/Exercise1a.java
@@ -0,0 +1,14 @@
+class Exercise1a{
+	public static void main(String[] args){
+		
+		int x = 1;
+		
+		while (x < 10){
+			x = x + 1; //forver loop without this line//
+			if (x > 3){
+				System.out.println("BIG X");
+			}
+			
+		}
+	}
+}
