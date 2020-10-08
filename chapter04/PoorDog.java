37ee2f507f60c5843468fe15b4f35db6dc22aff8
Heuristic <codemonkey2468@hotmail.com>
2020-10-08 10:08:32 -0400

    update chapter 4

---
+++ chapter04/PoorDog.java
@@ -0,0 +1,14 @@
+public class PoorDog{
+	private int size;
+	private String name;
+	//int size = 420;
+	//String name = "Donny";
+	
+	public int getSize(){
+		return size;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
