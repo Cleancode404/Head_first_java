1f2a9c852d313a55fb23711a859a9d7d5088b2fb
Heuristic <codemonkey2468@hotmail.com>
2020-10-12 14:26:25 -0400

    update chapter 6

---
+++ chapter06/DotCom.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class DotCom{
+	private ArrayList<String> locationCells;
+	private String name;
+	
+	public void setLocationCells(ArrayList<String> loc){
+		locationCells = loc;
+	}
+	
+	public void setName(String n){
+		name = n; //indentation error?
+	}
+	
+	public String CheckYourself(String userInput){
+		String result = "miss";
+		int index = locationCells.indexOf(userInput);
+		if (index >=0){
+			locationCells.remove(index);
+			
+			if (locationCells.isEmpty()){
+				result = "kill";
+				System.out.println("Ouch! You sunk " + name + " : ");
+				
+			}	else{
+				result = "hit";
+				
+			}//close if 
+		} //close if 
+		return result;
+	}//close method
+}//close class
