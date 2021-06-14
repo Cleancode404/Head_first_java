763c2afc25b112d9a01a3df40494a4c653e80952
Heuristic <codemonkey2468@hotmail.com>
2021-06-13 21:05:58 -0400

    correct OO

--- chapter02/DogLauncher.java
+++ chapter02/DogLauncher.java
@@ -1,14 +1,7 @@
 //call makenoise method in Dog.java from DogLauncher. This is OO
 
-//public class DogLauncher{
-//	public static void main(String[] args){
-//		Dog.makeNoise();
-//	}
-//}
-//
-//
-//public class Dog{
-//	public static void makeNoise(){
-//		System.out.println("Bark!");
-//	}
-//}
+public class DogLauncher{
+    public static void main(String[] args){
+        Dog.makeNoise();
+    }
+}
