15036051e7b4c7981bdaf77c4a7fd19a8a16e87b
Heuristic <codemonkey2468@hotmail.com>
2021-06-13 21:00:44 -0400

    correct style

--- Chapter01/Test1.java
+++ Chapter01/Test1.java
@@ -4,8 +4,8 @@ class Test1{
 		int y = 0;
 		while (x < 5){
 			y = x - y;
-			System.out.println(x +" " + y + "");
-			x = x +1;
+			System.out.println(x + " " + y + "");
+			x = x + 1;
 		}
 	}
 }
