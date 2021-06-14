a784494e5535b218ef1d75af899ab6999e19f7e3
Heuristic <codemonkey2468@hotmail.com>
2021-06-13 20:31:13 -0400

    correct typo

--- Chapter01/DooBee.java
+++ Chapter01/DooBee.java
@@ -3,14 +3,14 @@ public class DooBee{
 	public static void main(String[] args){
 		int x = 1;
 		while (x < 3){
-			System.out.println("Doo");
-			System.out.println("Bee");
+			System.out.print("Doo");
+			System.out.print("Bee");
 			x = x +1;
 			
 		}
 		
 		if (x == 3){
-			System.out.println("Do");
+			System.out.print("Do");
 		}
 	}
 }
