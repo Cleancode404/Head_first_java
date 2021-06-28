e3f344d542c397c0f20c849bf743b6456dce8b52
Heuristic <codemonkey2468@hotmail.com>
2021-06-28 11:37:24 -0400

    cont chapter 8

---
+++ Chapter08/MyAnimalList.java
@@ -0,0 +1,35 @@
+public class MyAnimalList {
+
+    private Animal[] animals = new Animal[5];
+    private int nextIndex = 0;
+
+    public void add(Animal a){
+        if (nextIndex < animals.length){
+            animals[nextIndex] = a;
+            System.out.println("Animal added at " + nextIndex);
+            nextIndex++;
+        }
+    }
+
+    
+}
+
+
+class Animal{
+    private int size;
+
+}
+
+class Dog extends Animal{
+    public void bark(){
+        System.out.println("Dog!");
+
+    }
+
+}
+
+class Cat extends Animal{
+    public void bark(){
+        System.out.print("Meow!");
+    }
+}
