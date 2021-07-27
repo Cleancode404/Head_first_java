52c58b205c2436c01276f1692a75d8d2c93c8733
Heuristic <codemonkey2468@hotmail.com>
2021-07-27 14:33:58 -0400

    cont chapter 11

--- Chapter11/ScaryException.java
+++ Chapter11/ScaryException.java
@@ -1,6 +1,13 @@
 public class ScaryException extends Exception {
 
-    public ScaryException(String exceptionMsg){     
-        System.out.println("in ScrayException: " + exceptionMsg);
+    static void doRisky(String test) throws ScaryException{
+        System.out.println("stat risky");
+        if ("yes".equals(test)) {
+            throw new ScaryException();
+
+        }
+        System.out.println("end risky ");
+        return;
+
     }
 }
